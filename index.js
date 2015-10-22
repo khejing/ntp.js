6599cbbf04e9b837b1a05e3aade61b1d80a31ffc
khejing <khejing@hotmail.com>
2015-10-22 17:35:30 +0800

    small bug fixed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import Moment from 'moment';
 
 //This file comes from: http://stackoverflow.com/questions/1638337/the-best-way-to-synchronize-client-side-javascript-clock-with-server-date#answer-22969338
 
+let MqttClientInThisFile = null;
 let c = null;
 let t0 = 0;
 
@@ -44,12 +45,13 @@ function onMessage(msg) {
 
         // log the calculated value rtt and time driff so we can manually verify if they make sense
         console.log("NTP delay: "+c.roundtripdelay+", NTP offset: "+c.offset+", corrected: "+Moment(t3 + c.offset).format("YYYY-MM-DD HH:mm:ss.SSS"));
-        MqttClient.offMessage(topic, "timesync", onMessage);
+        MqttClientInThisFile.offMessage(topic, "timesync", onMessage);
         resolve(c.offset);
     }
 }
 
 function getNTPOffset(MqttClient, topic){
+    MqttClientInThisFile = MqttClient;
     return new Promise(function(resolve, reject){
         if(c){
             console.log("have already got NTP delay: "+c.roundtripdelay+", NTP offset: "+c.offset+", so resolve directly");
